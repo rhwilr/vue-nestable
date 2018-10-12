851c8a413f4baca2063500fb47031c1684431da0
Ralph Huwiler <rhuwiler@hosttech.ch>
2018-10-12 14:20:42 +0200

    added examples

---
+++ examples/src/App.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class="app">
+    <simple/>
+    <advanced/>
+    <cross-list/>
+  </div>
+</template>
+
+<script>
+import Simple from '@/components/Simple.vue'
+import Advanced from '@/components/Advanced.vue'
+import CrossList from '@/components/CrossList.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Simple,
+    Advanced,
+    CrossList
+  }
+}
+</script>
+
+<style>
+/*
+* Style for examples
+*/
+body {
+    display: flex;
+    align-items: flex-start;
+    justify-content: center;
+    height: 100vh;
+    margin: 0;
+    background-color: #f3f3f4;
+}
+
+.app {
+    color: #444;
+    margin-top: 3em;
+    max-width: 800px;
+    flex: 1;
+    font-family: Helvetica, sans-serif;
+    display: flex;
+    flex-direction: column;
+}
+
+.component-example {
+    border-radius: 4px;
+    box-shadow: 0 2px 3px 0 rgba(16, 17, 23, .1), 0 0 1px 0 rgba(16, 17, 23, .1);
+    background-color: white;
+    color: #49494d;
+    padding: 1em;
+    flex: 1;
+    margin: 1em;
+}
+.side-by-side {
+  display: flex;
+}
+
+
+/*
+* Style for nestable
+*/
+.nestable {
+  position: relative;
+}
+.nestable .nestable-list {
+  margin: 0;
+  padding: 0 0 0 40px;
+  list-style-type: none;
+}
+.nestable > .nestable-list {
+  padding: 0;
+}
+.nestable-item,
+.nestable-item-copy {
+  margin: 10px 0 0;
+}
+.nestable-item:first-child,
+.nestable-item-copy:first-child {
+  margin-top: 0;
+}
+.nestable-item .nestable-list,
+.nestable-item-copy .nestable-list {
+  margin-top: 10px;
+}
+.nestable-item {
+  position: relative;
+}
+.nestable-item.is-dragging .nestable-list {
+  pointer-events: none;
+}
+.nestable-item.is-dragging * {
+  opacity: 0;
+  filter: alpha(opacity=0);
+}
+.nestable-item.is-dragging:before {
+  content: ' ';
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(106, 127, 233, 0.274);
+  border: 1px dashed rgb(73, 100, 241);
+  -webkit-border-radius: 5px;
+  border-radius: 5px;
+}
+.nestable-drag-layer {
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 100;
+  pointer-events: none;
+}
+.nestable-drag-layer > .nestable-list {
+  position: absolute;
+  top: 0;
+  left: 0;
+  padding: 0;
+  background-color: rgba(106, 127, 233, 0.274);
+}
+</style>
