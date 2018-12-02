6dd79815f30e202961721d9e26f9dbfd1a78b17b
Paul Bastowski <pbastowski@gmail.com>
2018-12-02 20:31:30 +0100

    Use slot-proxy pattern instead of custom _b

--- src/nestable-item.vue
+++ src/nestable-item.vue
@@ -19,8 +19,9 @@
           :item="item"
           :options="options"
           :is-copy="isCopy"
-          v-bind="{$scopedSlots}"
-        />
+          >
+            <template v-for="slot in Object.keys($scopedSlots)" :slot="slot" slot-scope="scope"><slot :name="slot" v-bind="scope"/></template>
+          </nestable-item>
 
       </template>
     </ol>
