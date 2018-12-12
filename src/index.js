f70d1d9219dc2e5fd7d7644697c81e75683a49fb
Ralph Huwiler <ralph@huwiler.rocks>
2018-12-12 22:21:04 +0100

    export components as named exports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import nestable from './nestable.vue'
 import nestableHandle from './nestable-handle.vue'
 
-module.exports = {
+// export named components
+export {
+  nestable as Nestable,
+  nestableHandle as NestableHandle
+}
+
+// export vue component installer
+export default {
   install: function (Vue, options) {
     Vue.component('vue-nestable', nestable)
     Vue.component('vue-nestable-handle', nestableHandle)
