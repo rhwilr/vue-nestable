abe40bcda2211dc8d6bceede78a1bebe61cd658b
punpunm <punpunm@hotmail.com>
2020-08-26 18:35:25 +0800

    Fix browser compatibility (#98)
    * Fix browser compatibility
    Event.srcElement is deprecated.

--- src/NestableItem.vue
+++ src/NestableItem.vue
@@ -146,7 +146,8 @@ export default {
       // when the mouse enters the item we save the size of this item
       // is is to improve performance, so we do not recalculate the size on every move
       this.moveDown = event.movementY > 0
-      this.breakPoint = event.srcElement.getBoundingClientRect().height / 2
+
+      this.breakPoint = event.target.getBoundingClientRect().height / 2
     },
     onMouseLeave () {
       this.breakPoint = null
